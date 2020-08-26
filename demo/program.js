65e62b473f6b032c011ddcf046cc3f46a118e361
liyu <476619600@qq.com>
2020-08-26 10:13:39 +0800

    javscript add methods

--- demo/program.js
+++ demo/program.js
@@ -1,1 +1,59 @@
 document.writeln("Hello world");
+
+
+Function.prototype.method = function(name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+    }
+    return this;
+};
+
+var a = ['a', 'b', 'c'];
+
+var b = ['x', 'y', 'z'];
+
+var c = a.concat(b, true);
+
+document.writeln('concat', ' ',c, "<br/>");
+
+
+var a = ['a', 'b', 'c'];
+a.push('d');
+
+
+var c = a.join('');
+document.writeln('join', ' ', c, "<br/>");
+
+var a = ['a', 'b', 'c'];
+
+var c = a.pop();
+
+document.writeln('pop', ' ', c, "<br/>");
+
+
+Array.method('pop', function () {
+    return this.splice(this.length - 1, 1)[0];
+});
+
+var d = a.pop();
+document.writeln('pop', ' ', d, "<br/>");
+
+var a = ['a', 'b', 'c'];
+var b = ['x', 'y', 'z'];
+
+var c = a.push(b, true);
+
+document.writeln('push', ' ', c, "<br/>");
+
+Array.method("push", function () {
+    this.splice.apply(this, [this.length, 0].concat(Array.prototype.slice.apply(arguments)));
+    return this.length;
+});
+
+
+var a = ['a', 'b', 'c'];
+var b = ['x', 'y', 'z'];
+
+var c = a.push(b, true);
+
+document.writeln('push', ' ', c, "<br/>");
--- reg/example/main.js
+++ reg/example/main.js
@@ -13,3 +13,41 @@ var i;
 for (i = 0; i < names.length; i += 1) {
     document.writeln(names[i] + ':' + blanks.substring(names[i].length), result[i], "<br/>");
 }
+
+// 一个匹配数字的正则表达式。数字可能由一个整数部分加上一个可选的负号、一个可选的小数部分和一个
+// 可选的指数部分组成
+
+var parse_number = /^-?\d+(?:\.\d*)?(?:e[+\-]?\d+)?$/i;
+
+var test = function (num) {
+    document.writeln(parse_number.test(num), "<br/>");
+};
+
+
+test('1'); // true
+test("number"); // false 
+test("98.6"); // true
+test("132.21.86.100"); // false;
+test('123.45E-67'); // true;
+test('123.45D-67'); // false
+
+
+// RegExp
+var my_regexp = new RegExp("\"(?:\\\\.|[^\\\\\\\"])*\"", 'g');
+
+function make_a_matcher() {
+    return /a/gi;
+};
+
+var x = make_a_matcher();
+
+var y = make_a_matcher();
+
+x.lastIndex = 10;
+
+document.writeln(y.lastIndex);
+
+
+// 元素
+
+"into".match(/in|int/);
